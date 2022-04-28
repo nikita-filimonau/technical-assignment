c920c904fa82c1615006f6fe151bedb55b202ebe
Nikita Filimonau <nikita.filimonau@leverx.com>
2022-04-29 00:59:40 +0300

    Add main settings

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import router from './router.js';
+import store from './store/index.js';
+import App from './App.vue'
+import BaseCard from './components/ui/BaseCard.vue';
+import BaseButton from './components/ui/BaseButton.vue';
+import BaseSpinner from './components/ui/BaseSpinner.vue';
+
+const app = createApp(App)
+
+app.use(router);
+app.use(store);
+
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+app.component('base-spinner', BaseSpinner);
+
+app.mount('#app');
