0cfe57b8aad95ae29ebcf9c2ea845d4b179d3fee
Nikita Filimonau <nikita.filimonau@leverx.com>
2022-04-29 01:00:09 +0300

    Add api constants

---
+++ src/constants/api.js
@@ -0,0 +1,11 @@
+const API_PREFIX = 'api/v1/';
+
+const API = {
+  MAIN: 'https://february-21.herokuapp.com/',
+  LOGIN: 'api-token-auth/',
+  QUOTES: `${API_PREFIX}quotes/`,
+  SUPPLIERS: `${API_PREFIX}suppliers/`,
+  USERS: `${API_PREFIX}users/`
+};
+
+export default API;
