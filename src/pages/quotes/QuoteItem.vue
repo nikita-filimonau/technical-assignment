058833b0e9d7be8253a3f90a66fbc0dbf695f241
Nikita Filimonau <nikita.filimonau@leverx.com>
2022-04-29 01:04:18 +0300

    Add quoute page

---
+++ src/pages/quotes/QuoteItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <li>
+    <h3>{{ title }}</h3>
+    <p>Amount: {{ amount }}</p>
+    <p>Created: {{ created }}</p>
+    <div class="actions">
+      <base-button @click="goToSupplier">Check Supplier</base-button>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['id', 'title', 'amount', 'created', 'supplier_id'],
+  methods: {
+    goToSupplier(){
+      this.$router.push(`/suppliers/${this.supplier_id}`)
+    }
+  }
+};
+</script>
+
+<style scoped>
+li {
+  margin: 1rem 0;
+  border: 1px solid #424242;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+h3 {
+  font-size: 1.5rem;
+}
+
+h3,
+h4 {
+  margin: 0.5rem 0;
+}
+
+div {
+  margin: 0.5rem 0;
+}
+
+.actions {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
---
+++ src/pages/quotes/QuotesList.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <section>
+      <base-card>
+        <div class="controls">
+          <base-button mode="outline" @click="loadPage">Refresh</base-button>
+          <base-pagination
+              :page="page"
+              :pages="pages"
+              @change-page="changePage"
+          ></base-pagination>
+        </div>
+        <div v-if="isLoading">
+          <base-spinner></base-spinner>
+        </div>
+        <ul v-else-if="hasQuotes">
+          <quote-item
+            v-for="quote in quotes"
+            :key="quote.id"
+            :id="quote.id"
+            :title="quote.title"
+            :amount="quote.amount"
+            :created="quote.created"
+            :supplier_id="quote.supplier_id"
+          ></quote-item>
+        </ul>
+        <h3 v-else>No Suppliers found.</h3>
+      </base-card>
+    </section>
+  </div>
+</template>
+
+<script>
+import QuoteItem from './QuoteItem';
+import { mapActions, mapGetters } from 'vuex';
+import BasePagination from "@/components/ui/BasePagination";
+export default {
+  data() {
+    return {
+      page: 1,
+    };
+  },
+  components: {
+    QuoteItem,
+    BasePagination,
+  },
+  watch: {
+    page(curValue, oldValue){
+      if (curValue && curValue !== oldValue) {
+        this.loadPage();
+      }
+    },
+  },
+  created() {
+    this.loadPage();
+  },
+  computed: {
+    ...mapGetters('quotes', ['quotes', 'isLoading', 'hasQuotes', 'count']),
+    pages(){
+      return Math.ceil(this.count / 10);
+    }
+  },
+  methods: {
+    ...mapActions('quotes', ['loadQuotes']),
+    changePage(page){
+      this.page = page;
+    },
+    loadPage() {
+      this.loadQuotes({ page: this.page });
+    }
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
